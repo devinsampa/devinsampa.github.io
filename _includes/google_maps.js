2333e371054d0b8927e52844f1bb61c0fae556c2
tinogomes <tinorj@gmail.com>
2013-03-09 10:08:58 -0300

    arrumando o link para o mapa

--- _includes/google_maps.js
+++ _includes/google_maps.js
@@ -1,10 +1,10 @@
-$(function() {
-  $("#ver-no-google").click(function() {
-    var google_maps_iframe = '<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt&amp;geocode=&amp;q=Av.+das+Na%C3%A7%C3%B5es+Unidas,+12.399&amp;aq=&amp;sll=-23.564794,-46.702816&amp;sspn=0.011388,0.01929&amp;gl=BR&amp;ie=UTF8&amp;hq=&amp;hnear=Av.+das+Na%C3%A7%C3%B5es+Unidas,+12399+-+Itaim+Bibi,+S%C3%A3o+Paulo,+04795-100&amp;t=m&amp;z=14&amp;ll=-23.606811,-46.697008&amp;output=embed"></iframe>';
-
-    $("#local address").after('<div style="margin: 20px 0">'+google_maps_iframe+'</div>');
-    $("#ver-no-google").remove();
-
-    return false;
-  });
-});
+// $(function() {
+//   $("#ver-no-google").click(function() {
+//     var google_maps_iframe = '<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt&amp;geocode=&amp;q=Av.+das+Na%C3%A7%C3%B5es+Unidas,+12.399&amp;aq=&amp;sll=-23.564794,-46.702816&amp;sspn=0.011388,0.01929&amp;gl=BR&amp;ie=UTF8&amp;hq=&amp;hnear=Av.+das+Na%C3%A7%C3%B5es+Unidas,+12399+-+Itaim+Bibi,+S%C3%A3o+Paulo,+04795-100&amp;t=m&amp;z=14&amp;ll=-23.606811,-46.697008&amp;output=embed"></iframe>';
+// 
+//     $("#local address").after('<div style="margin: 20px 0">'+google_maps_iframe+'</div>');
+//     $("#ver-no-google").remove();
+// 
+//     return false;
+//   });
+// });
